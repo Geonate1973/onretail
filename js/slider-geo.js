90223c6ea8ade034cd6014dc1d920e59022e7411
Geonate1973 <geonate@gmail.com>
2018-10-30 18:56:10 -0500

    segundo envio

--- js/slider-geo.js
+++ js/slider-geo.js
@@ -1,32 +1,11 @@
-//almacenar slider en 1 variable
-var slider=$('#slider');
-var siguiente=$('#btn-next');
-var anterior=$('#btn-prev');
+$(document).ready(function(){
 
+	$('.flexslider').flexslider({
+		prevText: "",
+		nextText: "",
+		pauseOnAction: false,
+		slideshowSpeed: 10000,
+		touch: true
+	});
 
-//mover ultima imagen al primer lugar
-$('#slider section:last').insertBefore('#slider section:first');
-
-slider.css('margin-left', '-'+100+'%');
-
-function moverD() {
-    slider.animate({marginLeft:'-'+200+'%'},700,function(){
-        $('#slider section:first').insertAfter('#slider section:last');
-        slider.css('margin-left', '-'+100+'%');
-    });
-}
-
-function moverI() {
-    slider.animate({marginLeft:0},700,function(){
-        $('#slider section:last').insertBefore('#slider section:first');
-        slider.css('margin-left', '-'+100+'%');
-    });
-}
-
-siguiente.on('click',function(){
-    moverD();
-})
-
-anterior.on('click',function(){
-    moverI();
-})
+});
